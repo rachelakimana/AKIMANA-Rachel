24bfee56ca8af7d27cb7bf19aab87f4e144e9c2d
AKIMANA Rachel <rachelakimana@gmail.com>
2021-01-28 17:17:56 +0200

    initial layout

---
+++ rachel-profile/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-white flex flex-row h-screen w-screen">
+      <div className="bg-blue-900 h-full w-1/4">
+      </div>
+      <div className="bg-yellow-700 flex-grow">
+
+      </div>
+
+    </div>
+    
+  );
+}
+
+export default App;
